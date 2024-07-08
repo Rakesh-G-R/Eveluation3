90ad52a6203236b069ad2ac830e0d4c83c460e23
RAKESH GOWDA <rakeshgowda495758@gmail.com>
2024-07-08 19:18:47 +0530

    commit5

--- src/config/sqlDB.js
+++ src/config/sqlDB.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 config();
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.SQL_USER, process.env.PASSWORD, {
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.SQL_USER, process.env.SECRETKEY, {
     host: process.env.SQL_HOST,
     dialect: 'mysql',
     port:process.env.SQL_PORT ,
